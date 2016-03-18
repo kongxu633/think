ef002a2c17ac9571e7426c495c9191b4d29e7cd7
kongxu633 <kongxu633@gmail.com>
2016-03-18 17:17:44 +0800

    test

--- Index/Conf/config.php
+++ Index/Conf/config.php
@@ -1,6 +1,16 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	SHOW_PAGE_TRACE => true, 
+	'SHOW_PAGE_TRACE' => true, 
+
+	//数据连接参数
+    'DB_TYPE'               => 'mysql',     // 数据库类型
+    'DB_HOST'               => 'localhost', // 服务器地址
+    'DB_NAME'               => 'lovewall',          // 数据库名
+    'DB_USER'               => 'root',      // 用户名
+    'DB_PWD'                => 'root',          // 密码
+    'DB_PORT'               => '8889',        // 端口
+    'DB_PREFIX'             => 'hd_',    // 数据库表前缀
+
 );
 ?>
